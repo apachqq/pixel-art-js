58c02d28a6f13f093ba446b0c1508ba6f03d95cc
apachqq <work.tsyganov@mail.ru>
2022-03-04 04:00:58 +0300

    Loop and function

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let pixels = document.querySelectorAll('.pixel');
 let color = document.querySelector('.chosen-color');
 let lastik = document.querySelector('.eraser');
+
+for (let pixel of pixels) {
+    pixel.onclick = function () {
+        if (lastik.checked === false) {
+            pixel.style.backgroundColor = color.value;
+        } else {
+            pixel.style.backgroundColor = 'white';
+        }
+    }
+}
